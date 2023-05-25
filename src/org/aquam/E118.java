a17406ed7aa62e39c4bd9bb2efddd507e450c1c4
Marina Tchernyshenko <aquamarina.na@gmail.com>
2023-05-25 21:02:13 +0300

    Add E118

--- src/org/aquam/E118.java
+++ src/org/aquam/E118.java
@@ -1,13 +1,14 @@
 package org.aquam;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+// 5/24/2023
+//
 public class E118 {
 
     public static void main(String[] args) {
-        System.out.println(generate(1));
+        //System.out.println(generate(1));
         System.out.println(generate(5));
     }
 
@@ -18,15 +19,17 @@ public class E118 {
             List<Integer> rowList = new ArrayList<>();
             List<Integer> previousList = pascalsTriangle.get(pascalsTriangle.size() - 1);
             rowList.add(0, previousList.get(0));
-            rowList.add(row - 1, previousList.get(previousList.size() - 1));
 
-            for (int rowLength = 0; rowLength < row; ++rowLength) {
-                int i = previousList.get(row - 1) + previousList.get(row);
-                rowList.add(row, i);
+            for (int rowLength = 1; rowLength < row - 1; ++rowLength) {
+                int i = previousList.get(rowLength - 1) + previousList.get(rowLength);
+                rowList.add(rowLength, i);
             }
+            rowList.add(row - 1, previousList.get(previousList.size() - 1));
 
             pascalsTriangle.add(rowList);
         }
         return pascalsTriangle;
     }
 }
+
+
